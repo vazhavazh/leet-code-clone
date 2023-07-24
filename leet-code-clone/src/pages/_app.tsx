8b237855937136c2be836313471509f3d7145105
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-07-24 22:57:22 +0300

    init and navbar

---
+++ leet-code-clone/src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+export default function App({ Component, pageProps }: AppProps) {
+	return (
+		<>
+			<Head>
+				<title>Leet Code Clone</title>
+				<meta
+					name='description'
+					content='Web applications tat contains leetcode problems and video solutions'
+				/>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1'
+				/>
+				<link
+					rel='icon'
+					href='/favicon.ico'
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
+}
---
+++ leet-code-clone/src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<>
+			<main>
+				<h1>hello world</h1>
+			</main>
+		</>
+	);
+}
