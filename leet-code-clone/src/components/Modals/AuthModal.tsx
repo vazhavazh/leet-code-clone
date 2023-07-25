008ccec93183ba1bb7c827fa30157dfcf7340090
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-07-25 12:20:11 +0300

    login ui

--- leet-code-clone/src/components/Modals/AuthModal.tsx
+++ leet-code-clone/src/components/Modals/AuthModal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { IoClose } from "react-icons/io5";
 import Login from "./Login";
+import SignUp from "./SignUp";
 type AuthModalProps = {};
 
 const AuthModal: React.FC<AuthModalProps> = () => {
@@ -26,7 +27,7 @@ const AuthModal: React.FC<AuthModalProps> = () => {
 								<IoClose className='h-5 w-5' />
 							</button>
 						</div>
-                        <Login />
+						<SignUp />
 					</div>
 				</div>
 			</div>
