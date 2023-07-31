aac19f1d65488604f216d6c0cccbfc124b52c53a
vazhavazh <118344739+vazhavazh@users.noreply.github.com>
2023-07-31 23:10:27 +0300

    SSR added

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import Split from "react-split";
 import ProblemDescription from "./ProblemDescription/ProblemDescription";
 import PlayGround from "./PlayGround/PlayGround";
-type WorkspaceProps = {};
+import { Problem } from "@/utils/types/problem";
+type WorkspaceProps = {
+	problem: Problem;
+};
 
-const Workspace: React.FC<WorkspaceProps> = () => {
+const Workspace: React.FC<WorkspaceProps> = ({ problem }) => {
 	return (
 		<Split
 			className='split'
 			minSize={0}>
-			<ProblemDescription />
+			<ProblemDescription problem={problem} />
 			<PlayGround />
 		</Split>
 	);
